9554fb747bd1a14c491a6d2d8594c983d1db56ed
TheRoro <39977138+TheRoro@users.noreply.github.com>
2021-02-26 09:55:22 -0500

    ✨ Created Rotating Page
    Created Rotating Faces/Images Page with image input and speed range.

---
+++ script.js
@@ -0,0 +1,15 @@
+const changeImage = (input) => {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#image').attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+
+const updateSpeed = (e) => {
+    let value = $("#val").val();
+    value = 5/value;
+    document.getElementById("image").style.animationDuration = `${value.toString()}s`;
+}
